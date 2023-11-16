dad1c1201df843b4775a5b2c7437c0075697e468
Frances Zhu <148593376+Franceesz@users.noreply.github.com>
2023-11-15 23:52:35 -0600

    Update main.py

--- PyPoll/main.py.py
+++ PyPoll/main.py.py
@@ -1,22 +1,23 @@
+#import modules
 import os
 import csv
-
+#set local pc path to data csv file
 csv_path=os.path.join("election_data.csv")
 
 with open(csv_path,'r') as csvfile:
     csvreader=csv.reader(csvfile,delimiter=',')
     header=next(csvreader)
-
+    #set lists
     total_v=[]
     candidate_t=[]
     clear_can=[]
     vote_count = []
     vote_percent = []
+    #run through the file
     for row in csvreader:
         total_v.append(int(row[0]))
         total_v_count=len(total_v)
         candidate_t.append(row[2])
-
 
     for x in set(candidate_t):
        clear_can.append(x)
@@ -24,11 +25,11 @@ with open(csv_path,'r') as csvfile:
        vote_count.append(y)
        z=(y/len(candidate_t))*100
        vote_percent.append(z)
-   
+   #use index
     max_vote=max(vote_count)
     winner=clear_can[vote_count.index(max_vote)]
     
-
+#print to terminal
 print("Election Results")
 print("------------------------")
 print(f"Total Votes: {total_v_count}")
@@ -39,7 +40,7 @@ print("-------------------------")
 print(f"Winner: {winner}")
 print("-------------------------")
 
-
+#write csv file
 with open("output.txt","wt") as new:
     new.write("Election Results\n")
     new.write("------------------------\n")
